05bd4cb857395db326fd0da54646c6a03cbd9abb
Nikita Polunin (二木立・半人) <Stkchanelx@gmail.com>
2022-02-10 13:54:48 +0300

    Добавьте файлы проекта.

---
+++ MyQueue/OtherQueue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyQueue
+{
+    internal class OtherQueue : IQueue
+    {
+        public List<int> queue = new();
+
+        public int Dequeue()
+        {
+            int temp = queue.First();
+
+            queue.RemoveAt(0);
+
+            return temp;
+        }
+
+        public void Enqueue(int item)
+        {
+            queue.Add(item);
+        }
+
+        public int Peek()
+        {
+            return queue.First();
+        }
+    }
+}
---
+++ MyQueue/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MyQueue
+{
+    class Programm
+    {
+        static void Main()
+        {
+            OtherQueue ew = new();
+
+            ew.Enqueue(546);
+            ew.Enqueue(53);
+            ew.Enqueue(12);
+
+            ew.Dequeue();
+            ew.Dequeue();
+
+            Console.WriteLine(ew.Peek());
+        }
+    }
+}
